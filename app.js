7ecf13f6e165d7b3c741298074bddfbb7c9a66fd
EPina <edgardopina57@gmail.com>
2023-12-07 01:16:23 -0700

    Small refactoring

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-//* slice - creates subarray
-//* process - equivalent to DOM
-//* process.argv - parametros in the command line
 var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log("process.argv:", process.argv);
 
-//* arrow function
 const printProfileData = (profileDataArr) => {
-   // for (let i = 0; i < profileDataArr.length; i++) {
-   //    console.log(profileDataArr[i]);
-   // }
-   //* note array.forEach() method syntax and arrow function to process each element
    profileDataArr.forEach((profileItem) => {
       console.log(profileItem);
    });
