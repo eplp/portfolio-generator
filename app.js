43bd98ebb1817c2e8f2a1cbebf30882dd378e593
EPina <edgardopina57@gmail.com>
2023-12-07 02:44:25 -0700

    Added de-structuring variables, added generatePage function to create html code, added fs.writeFile() to create index.html

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const fs = require('node:fs');
 var profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-   profileDataArr.forEach((profileItem) => {
-      console.log(profileItem);
-   });
+const [profileName, github] = profileDataArgs; //* destructuring
+console.log('profileName:', profileName);
+
+const generatePage = (profileName, github) => {
+   return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${profileName}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(profileName, github), (err) => {
+   if (err) {
+      throw err;
+   }
+   console.log('Portfolio complete! Check out index.html to see the output!');
+});
