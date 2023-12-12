b20ae803b78727802a0d44c08a1ee88981ca0cd6
EPina <edgardopina57@gmail.com>
2023-12-12 12:32:32 -0700

    Added "type": "module" ton package.json to run inquirer as ESM module. Added code to test each inquirer prompt type

--- app.js
+++ app.js
@@ -1,11 +1,80 @@
-   // const fs = require('node:fs/promises');
-   const fs = require('fs');
-   const generatePage = require('./src/page-template')
+// const inquirer = require('inquirer');  //* dos NOT work anymore
+// import inquirer from 'inquirer'; //* requires app.js be renamed to app.mjs
 
-   const profileDataArgs = process.argv.slice(2);
-   const [profileName, github] = profileDataArgs; //* destructuring
+import inquirer from 'inquirer'; //* requires to add "type": "module" in the package.json file
 
-   fs.writeFile('index.html', generatePage(profileName, github), (err) => {
-      if (err) throw new Error(err);
-      console.log('Portfolio complete! Check out index.html to see the output!');
-   });
+// const fs = require('node:fs/promises');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template')
+
+// const profileDataArgs = process.argv.slice(2);
+// const [profileName, github] = profileDataArgs; //* destructuring
+
+// fs.writeFile('index.html', generatePage(profileName, github), (err) => {
+//    if (err) throw new Error(err);
+//    console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+   .prompt([
+      // {
+      //    type: 'input',
+      //    name: 'name',
+      //    message: "What's your name?",
+      // },
+      // {
+      //    type: 'number',
+      //    name: 'birthYear',
+      //    message: 'What year you were born?',
+      // },
+      // {
+      //    type: 'confirm',
+      //    name: 'areSure',
+      //    message: 'Are you sure? ',
+      // },
+      // {
+      //    type: 'list',
+      //    name: 'friendsList',
+      //    message: 'Select your best friend: ',
+      //    choices: ['Sam', 'Jairo', 'Oswald', 'Rodrigo'],
+      // },
+      // {
+      //    type: 'rawlist',
+      //    name: 'friendsList',
+      //    message: 'Select your best friend: ',
+      //    choices: ['Sam', 'Jairo', 'Oswald', 'Rodrigo'],
+      // },
+      // {
+      //    type: 'expand',
+      //    name: 'expanded',
+      //    message: "I don' know what to ask: ",
+      //    choices: [
+      //       {
+      //          key: 'o',
+      //          name: 'Overwrite',
+      //          value: 'overwrite',
+      //       },
+      //       {
+      //          key: 'b',
+      //          name: 'Bad',
+      //          value: 'badbad',
+      //       },
+      //    ],
+      // },
+      // {
+      //    type: 'checkbox',
+      //    name: 'myCheck',
+      //    choices: ['The Bible', 'The Book of Mormon', 'D&C', 'The Pearl of Great Price'],
+      // },
+      // {
+      //    type: 'password',
+      //    name: 'myPassword',
+      //    message: 'Enter password: ',
+      //    mask: '.',
+      // },
+      {
+         type: 'editor',
+         name: 'myEditorText',
+         message: 'Describe the situation: ',
+      },
+   ])
+   .then((ans) => console.log(ans));
