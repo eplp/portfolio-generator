9595685f2ebb434fae0f3371fabe7717470922a1
EPina <edgardopina57@gmail.com>
2023-12-12 00:15:09 -0700

    Uninstall inquirer

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-//* const fs = require('node:fs/promises');
-const fs = require('fs');
-const generatePage = require('./src/page-template')
+   // const fs = require('node:fs/promises');
+   const fs = require('fs');
+   const generatePage = require('./src/page-template')
 
-const profileDataArgs = process.argv.slice(2);
-const [profileName, github] = profileDataArgs; //* destructuring
+   const profileDataArgs = process.argv.slice(2);
+   const [profileName, github] = profileDataArgs; //* destructuring
 
-fs.writeFile('index.html', generatePage(profileName, github), (err) => {
-   if (err) throw new Error(err);
-   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+   fs.writeFile('index.html', generatePage(profileName, github), (err) => {
+      if (err) throw new Error(err);
+      console.log('Portfolio complete! Check out index.html to see the output!');
+   });
